696f0039188e5a04284477589f08a3fa8039abc7
KC Fong <kcfongdesign@gmail.com>
2025-03-27 18:19:19 -0700

    added basic global styles. added styletext.jsx just for VISUAL reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
+import StyleTest from "./StyleTest";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<StyleTest />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/StyleTest.jsx
@@ -0,0 +1,22 @@
+import "./styles/partials/_global.scss";
+
+function StyleTest() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <h2>Sub-Header</h2>
+      <h3>labes/links/buttons</h3>
+      <h4>table Header</h4>
+      <p>body </p>
+      <button className="button__main"> Button Main Test</button>
+      <button className="button__secondary"> Button Secondary Test</button>
+      <button className="button__delete"> Button Delete Test</button>
+
+    <div className="container">Basic Ass Container</div>
+
+
+    </div>
+  );
+}
+
+export default StyleTest;
