8db93f33381e0ef8090cabd588394e803a02d459
KC Fong <kcfongdesign@gmail.com>
2025-03-29 09:43:56 -0700

    Added Navigation.jsx, updated _global.scss button styles, added svgr package to vit.config.js for vectors to be used

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+
+import Logo from "../../assets/Logo/InStock-Logo.svg?react"
+
+export const Navigation = () => {
+  return (
+    <>
+      <nav className="nav">
+        <Logo />
+       
+      </nav>
+    </>
+  );
+};
+
+export default Navigation;
+
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,24 @@
-
+import Navigation from "../../components/Navigation";
 
 const HomePage = () => {
   return (
+    <>
     <div>
       <h1>Header</h1>
       <h2>Sub-Header</h2>
       <h3>labes/links/buttons</h3>
       <h4>table Header</h4>
       <p>body </p>
-      <button className="button__main"> Button Main Test</button>
-      <button className="button__secondary"> Button Secondary Test</button>
-      <button className="button__delete"> Button Delete Test</button>
+      <button className="btn-main"> Button Main Test</button>
+      <button className="btn-secondary"> Button Secondary Test</button>
+      <button className="btn-delete"> Button Delete Test</button>
 
-    <div className="container">Basic Ass Container</div>
-
-
+      <div className="container">Basic Ass Container</div>
     </div>
-  )
-}
 
-export default HomePage
+    <Navigation />
+    </>
+  );
+};
+
+export default HomePage;
