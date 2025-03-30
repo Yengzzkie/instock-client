8e586c3ae25bb67da4c6e8877f4037d83c3bc98f
Yengzzkie <gatchalian.manuel@gmail.com>
2025-03-30 00:02:55 -0400

    added table component

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,79 @@
+import "./Table.scss";
+import Delete from "../../assets/Icons/delete_outline-24px.svg?react";
+import Edit from "../../assets/Icons/edit-24px.svg?react";
+import Sort from "../../assets/Icons/sort-24px.svg?react";
+
+const TABLE_HEAD = [
+  "INVENTORY ITEM",
+  "Category",
+  "Status",
+  "Quantity",
+  "Actions",
+];
+
+const TABLE_DATA = [
+  { item: "Laptop", category: "Electronics", status: "In Stock", quantity: 10 },
+  {
+    item: "Office Chair",
+    category: "Furniture",
+    status: "Low Stock",
+    quantity: 3,
+  },
+  {
+    item: "Notebook",
+    category: "Stationery",
+    status: "Out of Stock",
+    quantity: 0,
+  },
+  {
+    item: "Headphones",
+    category: "Electronics",
+    status: "In Stock",
+    quantity: 25,
+  },
+  {
+    item: "Whiteboard",
+    category: "Office Supplies",
+    status: "In Stock",
+    quantity: 7,
+  },
+];
+
+const Table = () => {
+  return (
+    <div className="table__container">
+
+      <table>
+        {/* TABLE HEADER */}
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head, index) => (
+              <th key={index}>
+                <td>
+                  {head.toUpperCase()} <Sort />
+                </td>
+              </th>
+            ))}
+          </tr>
+        </thead>
+        {/* TABLE BODY */}
+        <tbody>
+          {TABLE_DATA.map((row, index) => (
+            <tr key={index}>
+              <td>{row.item}</td>
+              <td>{row.category}</td>
+              <td>{row.status}</td>
+              <td>{row.quantity}</td>
+              <td>
+                <Delete className="table__cta" />
+                <Edit className="table__cta" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
+import Table from "../../components/Table/Table";
 
 const HomePage = () => {
   return (
@@ -18,6 +19,7 @@ const HomePage = () => {
     </div> */}
 
     <Navigation />
+    <Table />
 
     <Footer />
     </>
