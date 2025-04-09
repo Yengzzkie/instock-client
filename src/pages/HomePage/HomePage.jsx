1fcef7872f1a8b563b346fd50119d1a281418981
KC Fong <kcfongdesign@gmail.com>
2025-04-08 18:24:59 -0700

    Made AddNewWarehouse component using the WarehouseEdit component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,11 @@
-import Navigation from "../../components/Navigation/Navigation";
-import Footer from "../../components/Footer/Footer";
-import WarehouseEdit from "../../components/WarehouseEdit/WarehouseEdit";
+import AddNewWarehouse from "../../components/AddNewWarehouse/AddNewWarehouse";
 
 const HomePage = () => {
   return (
     <>
-    {/* <div>
-      <h1>Header</h1>
-      <h2>Sub-Header</h2>
-      <h3>labes/links/buttons</h3>
-      <h4>table Header</h4>
-      <p>body </p>
-      <button className="btn-main"> Button Main Test</button>
-      <button className="btn-secondary"> Button Secondary Test</button>
-      <button className="btn-delete"> Button Delete Test</button>
+      {/* <h1>Hello from HomePage</h1> */}
 
-      <div className="container">Basic Ass Container</div>
-    </div> */}
-
-    <Navigation />
-    <WarehouseEdit />
-
-    <Footer />
+      <AddNewWarehouse/>
     </>
   );
 };
