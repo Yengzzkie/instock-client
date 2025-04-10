fce214439043bed6622a6e721f638f6301ab03d8
Diego <diegopinlac@gmail.com>
2025-04-09 20:24:17 -0700

    Fixed bugs (sass file not imported). Added the following fields: number, textarea, and radio. Changed dropdown type to select.

--- src/components/FormField/FormField.jsx
+++ src/components/FormField/FormField.jsx
@@ -1,15 +1,18 @@
+import "../FormField/FormField.scss";
+
 const FormField = ({input}) => {
   {/* 
     Usage:
     
-    1. import the component (e.g. import "../FormField/FormField.scss";)
+    1. import the component (e.g. import FormField from "../FormField/FormField.jsx";)
     2. Create an element by using the code below
 
     <FormField input={{
       label: "",
       class: "", // modifiers. (e.g. error)
-      type: "", // choices: dropdown, input, search
+      type: "", // choices: select, input, number, textarea, search, radio
       name: "", 
+      placeholder: "",
       value: "",
       // onChange: "", // function name
 
@@ -25,8 +28,11 @@ const FormField = ({input}) => {
   return (
     <>
       <div className="form__group">
-        <label className="form-label" htmlFor={input.name}>{input.label}</label>
-        {input.type === "dropdown" ? (
+        <label className="form-label" {...(input.type !== "radio" ? { htmlFor: input.name } : {})}>
+          {input.label}
+        </label>
+
+        {input.type === "select" ? (
           <select
             className={`form-input ${input.class}`}
             id={input.name}
@@ -40,16 +46,54 @@ const FormField = ({input}) => {
               </option>
             ))}
           </select>
-        ) : (
-          <input
-            className={`form-input ${input.class}`}
-            type={input.type}
+        ) : input.type === "textarea" ? (
+          <textarea
+            className={`form-input textarea ${input.class}`}
             id={input.name}
             name={input.name}
             placeholder={input.placeholder}
             defaultValue={input.value}
             onChange={input.onChange}
           />
+        ) : input.type === "input" ? (
+          <input
+            className={`form-input ${input.class}`}
+            id={input.name}
+            type={input.type}
+            name={input.name}
+            placeholder={input.placeholder}
+            defaultValue={input.value}
+            onChange={input.onChange}
+          />
+        ) : input.type === "number" ? (
+          <input
+            className={`form-input ${input.class}`}
+            id={input.name}
+            type={input.type}
+            name={input.name}
+            min="0"
+            placeholder={input.placeholder}
+            defaultValue={input.value}
+            onChange={input.onChange}
+          />
+        ) : input.type === "radio" ? (
+          <div className={`form-radio-group ${input.class}`}>
+            {input.options?.map((option, index) => (
+              <label key={index}>
+                <input
+                  className={`form-input`}
+                  type="radio"
+                  name={input.name}
+                  value={option.value}
+                  defaultChecked={input.value === option.value}
+                  onChange={input.onChange}
+                />
+                <span>{option.label}</span>
+              </label>
+            ))}
+          </div>
+        ) : (
+          <></>
         )}
         
         {input.class.includes('error') && (
