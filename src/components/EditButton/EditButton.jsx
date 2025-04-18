a494aeaf1ab4a71fb3d78e57ea3575bdb965de9c
Yengzzkie <gatchalian.manuel@gmail.com>
2025-04-18 16:53:25 -0400

    added descriptive usage for EditButton component

--- src/components/EditButton/EditButton.jsx
+++ src/components/EditButton/EditButton.jsx
@@ -2,6 +2,18 @@ import "./EditButton.scss";
 import { Link } from "react-router-dom";
 import EditWhite from "../../assets/Icons/edit-white-24px.svg?react";
 
+/** 
+ * USAGE:
+ * pass the path prop to the component to set the path of the button.
+ * 
+ * Example:
+ * 
+ * import EditButton from "./components/EditButton/EditButton";
+ * 
+ * <EditButton path={`/warehouse/edit/${id}`} />  
+ * 
+ * **/
+
 const EditButton = ({ path }) => {
   return (
     <Link to={path}>
